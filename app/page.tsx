954d646d5b06bbf21de0522256ca1c78d1a862f8
johnny2smooth <johnbp13@gmail.com>
2023-03-23 10:36:08 -0700

    next config set up to get images from supabase. supabase bucket made public so that I can fetch images

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,35 @@
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import CanvasDrawer from "./canvas-drawer";
+import { supabase } from "./supabase";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export const revalidate = 0;
+export default async function Home() {
+  let publicUrls: string[] = [];
+  const { data } = await supabase.storage
+    .from(process.env.SUPABASE_BUCKET!)
+    .list();
+
   return (
     <main className={styles.main} style={{ background: "black" }}>
       <CanvasDrawer />
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {data?.map((file) => (
+          <Image
+            key={file.id}
+            src={
+              "https://tkcaaymkxvrpewbwspwh.supabase.co/storage/v1/object/public/friend-drawings/" +
+              file.name
+            }
+            alt={file.name}
+            width={100}
+            height={100}
+          />
+        ))}
+      </div>
     </main>
   );
 }
--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    domains: [`tkcaaymkxvrpewbwspwh.supabase.co`],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
